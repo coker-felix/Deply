92fce0d8a60806ae02175455e0beb06be21c2406
Coker Felix <felixwood84@ymail.com>
2020-11-01 15:01:41 +0000

    modified predict

--- deply/predict.py
+++ deply/predict.py
@@ -1,10 +1,11 @@
 import os
 import numpy as np
 import tensorflow as tf
-from keras.applications.inception_v3 import preprocess_input
-from keras.models import load_model
+from tensorflow.keras.applications.inception_v3 import preprocess_input
+from tensorflow.keras.models import load_model
 from tensorflow.python.framework import ops
-from keras.preprocessing.image import img_to_array, load_img
+from tensorflow.keras.preprocessing.image import img_to_array, load_img
+from flask import Flask, redirect, url_for, request, render_template
 
 
 #Load_model
@@ -29,7 +30,7 @@ def model_predict(img_path):
     preds = Plas_spec.predict(image)
     a = (preds[0])
     total = sum(sum(preds))
-    result = 'Falciparum: {} - Malariae:  {} - Ovale: {} -Vivax: {}'.format(round(a[0]/total), round(a[1]/total), round(a[2]/total), round(a[3]/total))
+    result = 'Falciparum: {} - Malariae:  {} - Ovale: {} -Vivax: {}'.format(round(a[0]/total,3), round(a[1]/total,3), round(a[2]/total,3), round(a[3]/total,3))
 
     return result 
     # return 'Falciparum: ',round(a[0]/total),'\n Malariae: ' , round(a[1]/total), '\n Ovale: ',round(a[2]/total), '\n Vivax: ',round(a[3]/total)
